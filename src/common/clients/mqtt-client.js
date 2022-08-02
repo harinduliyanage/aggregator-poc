ec47a5e2c37beb6214740601fb8e81453517be49
Harindu Liyanage <harindu.sul@gmail.com>
2022-08-02 15:43:00 +0530

    written a mqtt client initial impl

---
+++ src/common/clients/mqtt-client.js
@@ -0,0 +1,54 @@
+/**
+ * @file - mqtt-client.js
+ * @description - generic mqtt client
+ */
+import * as mqtt from "mqtt";
+//
+import {logger} from "../index";
+
+class MqttClient {
+
+    constructor() {
+        this.client = {};
+    }
+
+    /**
+     * use to make connection establish with the server
+     * @param {string} url
+     * @param config
+     */
+    connect = (url, config) => {
+        try {
+            this.client = mqtt.connect(url, config);
+        } catch (e) {
+            logger.error(e);
+        }
+    }
+
+    /**
+     * use to subscribe for particular topic
+     * @param {string} topic
+     */
+    subscribe = (topic) => {
+        try {
+            this.client.subscribe(topic);
+        } catch (e) {
+            logger.error(e);
+        }
+    }
+
+    /**
+     * use to attach listeners for particular eventName
+     * @param eventName
+     * @param listener
+     */
+    addListener = (eventName, listener) => {
+        try {
+            this.client.addListener(eventName, listener);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
+
+export default MqttClient;
