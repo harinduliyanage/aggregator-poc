a6cf81cda4fc80e1c4881ef1c9295d847f685447
Harindu Liyanage <harindu.sul@gmail.com>
2022-08-02 16:40:21 +0530

    updated

--- src/common/index.js
+++ src/common/index.js
@@ -3,11 +3,15 @@ import MqttClient from "./clients/mqtt-client";
 import SocketClient from "./clients/socket-client";
 //
 import logger from "./utils/logger";
+import config from "./config/config";
 
 export {
+    // clients
     HttpClient,
     MqttClient,
     SocketClient,
-    //
-    logger
+    // logger service
+    logger,
+    // configurations
+    config
 }
