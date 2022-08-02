0fa225d58b7d30cafb0f8632e2f001566fb9e5b6
Harindu Liyanage <harindu.sul@gmail.com>
2022-08-02 16:23:49 +0530

    msg listeners skeleton

---
+++ src/message-listener/mqtt/mqtt-message-listener.js
@@ -0,0 +1,21 @@
+
+const mqttMessageListener =  {
+
+    connect: () => {
+        // todo: using mqtt client make socket connection establish
+        listener();
+    }
+}
+
+
+
+const listener = (rawMessage) => {
+    // todo: step 1 using parser decode the raw msg
+
+    // todo: step 2 get persist and other decision
+
+    // todo: step 3 persist
+
+
+
+}
---
+++ src/message-listener/socket/socket-message-listener.js
@@ -0,0 +1,16 @@
+
+const socketMessageListener =  {
+
+    connect: () => {
+        // todo: using socket client make socket connection establish
+        listener();
+    }
+}
+
+const listener = (socketMessage) => {
+    // todo: using parser decode the raw msg
+
+    // todo: step 2 get persist and other decision
+
+    // todo: step 3 persist
+}
