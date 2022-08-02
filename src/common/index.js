db869b4de00c67a35699323753a7ed47237190c1
Harindu Liyanage <harindu.sul@gmail.com>
2022-08-02 16:18:17 +0530

    expose point of common module

---
+++ src/common/index.js
@@ -0,0 +1,13 @@
+import HttpClient from "./clients/http-client";
+import MqttClient from "./clients/mqtt-client";
+import SocketClient from "./clients/socket-client";
+//
+import logger from "./utils/logger";
+
+export {
+    HttpClient,
+    MqttClient,
+    SocketClient,
+    //
+    logger
+}
