dadff0c7b16202a6db4bc7108f942b68be44bd24
Harindu Liyanage <harindu.sul@gmail.com>
2022-08-05 15:43:59 +0530

    updated bluetooth-scan-data model

--- src/message-parser/dto/bluetooth-scan-data.js
+++ src/message-parser/dto/bluetooth-scan-data.js
@@ -4,5 +4,20 @@ class BluetoothScanData {
         this.rssi = rssi;
         this.broadcastData = broadcastData;
         this.broadcastName = broadcastName;
+        this.deviceName = '';
+    }
+
+    setDeviceName(name) {
+        this.deviceName = name;
+    }
+
+    toString() {
+        return `BluetoothScanData {\n
+          macAddress: ${this.macAddress},\n
+          rssi: ${this.rssi},\n
+          broadcastData: ${this.broadcastData},\n
+          broadcastName: ${this.broadcastName},\n
+          deviceName: ${this.deviceName}\n
+        }`
     }
 }
