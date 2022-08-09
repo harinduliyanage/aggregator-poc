966471eb0ea2b56d0773349150e7b821b8f020d8
Harindu Liyanage <harindu.sul@gmail.com>
2022-08-09 17:04:29 +0530

    added cmt

--- src/message-parser/parser/mqtt-parser.js
+++ src/message-parser/parser/mqtt-parser.js
@@ -1,4 +1,8 @@
-import *as deviceManager from "../utils/device-manager";
+/**
+ * @file - mqtt parser
+ * @description - mqtt stream specific parser
+ */
+import * as deviceManager from "../utils/device-manager";
 import MokoMk103 from "../gateways/moko-mk103";
 
 export default class MqttParser {
@@ -10,9 +14,18 @@ export default class MqttParser {
     constructor(options = {devices: []}) {
         // registering system devices in parser context
         deviceManager.saveRegisteredDevices(options.devices);
+        // initializing gateway parser
         this.parser = new MokoMk103();
     }
 
+    /**
+     * msg parsing functionality
+     * @param rawMessage
+     * @return {{
+     * scannedBluetoothData: BluetoothScanData[], decodedData: DeviceLog[],
+     * qtyOfScanData: number, deviceId: string
+     * }}
+     */
     parse(rawMessage) {
         return this.parser.parse(rawMessage);
     }
