3a09092a3e3ef3e4f8cc3de22cebcde831cddc71
Harindu Liyanage <harindu.sul@gmail.com>
2022-08-09 14:00:49 +0530

    db url added

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -7,6 +7,7 @@
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 import * as Joi from 'joi';
+import {config} from "../index";
 
 dotenv.config({ path: path.join(__dirname, '../../../.env') });
 process.env.ROOT_PATH = path.join(__dirname, '../../../');
@@ -17,6 +18,10 @@ const envVarsSchema = Joi.object()
         PORT: Joi.number().default(3004),
         MQTT_HOST: Joi.string().required().description('Mqtt host address'),
         XK_SOCKET_URL: Joi.string().required().description('XK socket url'),
+        DB_USERNAME: Joi.string().required().description('Database username'),
+        DB_PASSWORD: Joi.string().required().description('Database password'),
+        DB_CLUSTER_URL: Joi.string().required().description('Database cluster url'),
+        DB_NAME: Joi.string().required().description('Database name'),
     }).unknown();
 
 // schema validation
@@ -25,11 +30,20 @@ const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' }
 if (error) {
     throw new Error(`configuration validation error: ${error.message}`);
 }
-
+//
+const DATABASE_URL = "mongodb+srv://"
+    + config.DB_USERNAME
+    + ":"
+    + config.DB_PASSWORD
+    + "@"
+    + config.DB_CLUSTER_URL
+    + "/"
+    + config.DB_NAME
+    + "?retryWrites=true&w=majority";
 // export config object
 export default {
     PORT: envVars.PORT,
     MQTT_HOST: envVars.MQTT_HOST,
     XK_SOCKET_URL: envVars.XK_SOCKET_URL,
-
+    DATABASE_URL: DATABASE_URL
 }
