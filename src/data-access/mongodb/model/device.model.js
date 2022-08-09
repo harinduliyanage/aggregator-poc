e13561c73e0845a6431f423a03230f64486e5f57
Harindu Liyanage <harindu.sul@gmail.com>
2022-08-09 13:59:02 +0530

    device.model.js created

---
+++ src/data-access/mongodb/model/device.model.js
@@ -0,0 +1,56 @@
+import * as mongoose from 'mongoose';
+import mongooseAutoPopulate from 'mongoose-autopopulate';
+//
+import toJSON from "./plugins/toJSON.plugin";
+import paginate from "./plugins/paginate.plugin";
+
+/** device model schema */
+const deviceSchema = mongoose.Schema({
+        isGateway: {
+            type: Boolean,
+            trim: true,
+        },
+        organizationId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Organizations',
+            trim: true,
+        },
+        deviceModel: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Device Models',
+            autopopulate: true
+        },
+        deviceName: {
+            type: String,
+            trim: true,
+        },
+        xkDeviceId: {
+            type: String,
+            trim: true,
+        },
+        macAddress: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        supportedReadingTypes: {
+            type: [{
+                type: String
+            }]
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+// add plugin that converts mongoose to json
+deviceSchema.plugin(toJSON);
+deviceSchema.plugin(paginate);
+deviceSchema.plugin(mongooseAutoPopulate);
+
+/** device model */
+const Device = mongoose.model('Device', deviceSchema)
+
+/** exporting the device model only for the usage within data access layer */
+export default Device;
