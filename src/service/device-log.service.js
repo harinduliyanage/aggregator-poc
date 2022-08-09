697029edb1a2ad15d454cb9b4e798fd1ce26912f
Harindu Liyanage <harindu.sul@gmail.com>
2022-08-09 16:51:16 +0530

    added cmt

--- src/service/device-log.service.js
+++ src/service/device-log.service.js
@@ -1,7 +1,16 @@
+/**
+ * @file device log service
+ * @summary device log domain related business logics
+ */
 import {RepositoryStore} from "../data-access";
 
 const deviceLogService = {
 
+    /**
+     * save multiple device logs at once
+     * @param {[DeviceLog]} deviceLogs
+     * @return {Promise<*>}
+     */
     saveList: async (deviceLogs) => {
         return RepositoryStore.DeviceLogRepository.saveList(deviceLogs);
     }
--- src/service/device-service.js
+++ src/service/device-service.js
@@ -1,7 +1,15 @@
+/**
+ * @file device service
+ * @summary device domain related business logics
+ */
 import {RepositoryStore} from "../data-access";
 
 const deviceService = {
 
+    /**
+     * get all registered devices
+     * @return {Promise<*[]>}
+     */
     getAllDevices: async () => {
         return RepositoryStore.DeviceRepository.find({});
     }
